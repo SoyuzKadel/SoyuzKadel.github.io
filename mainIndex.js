3bf5ecaca14f0ae8c388cf36d5f642c1077814af
SoyuzKadel <soyuzkadel@gmail.com>
2024-10-02 03:17:01 -0400

    size adjustment, chain

--- mainIndex.js
+++ mainIndex.js
@@ -1,3 +1,5 @@
+
+//Changing images bts for Problem 1
 function p01Func() {
     document.getElementById("problem").innerHTML="<p>I'm looking for a type of pancake originating from Indian subcontinent, made from a fermented batter. It is somewhat similar to a creepe in appearance.</p>";
     document.getElementById("flowchart").setAttribute("src","images/dosa/dosaDesign.jpg");
@@ -10,6 +12,9 @@ function p01Func() {
     document.getElementById("check2").checked=false;
     document.getElementById("check3").checked=false;
 }
+
+
+//Changing images bts for Problem 2     
 function p02Func() {
     document.getElementById("problem").innerHTML="<p>I'm looking for an Iranian dish that consists of grilled chunks of chicken which are sometimes with bone and other times without bone. It's one of the most popular dishes of Iran. It is common to marinate the chunks in minced onion, lemon juice and sometimes saffron.</p>";
     document.getElementById("flowchart").setAttribute("src","images/jujeh/jujehDesign.jpg");
@@ -23,6 +28,11 @@ function p02Func() {
     document.getElementById("check3").checked=false;
 }
 
+
+
+
+
+//Switch image display on/off for CHECKBOX 1 i.e. DESIGN
 function checkUncheck1(){
     if (document.getElementById("check1").checked==true) {
         
@@ -33,6 +43,8 @@ function checkUncheck1(){
     }
 }
 
+
+//Switch image display on/off for CHECKBOX 2 i.e. SOLUTION 1
 function checkUncheck2(){
     if (document.getElementById("check2").checked==true) {
       document.getElementById("js").style.display="inline";
@@ -42,6 +54,8 @@ function checkUncheck2(){
     }
 }
 
+
+//Switch image display on/off for CHECKBOX 3 i.e. SOLUTION 2
 function checkUncheck3() {
     if (document.getElementById("check3").checked==true) {
         document.getElementById("another").style.display="inline";
@@ -51,6 +65,8 @@ function checkUncheck3() {
     }
 }
 
+
+//Zoom in/out function ***Needs a Scroll Bar***
 function zoomIn() {
     document.getElementById("flowchart").style.width="200%";
 }
