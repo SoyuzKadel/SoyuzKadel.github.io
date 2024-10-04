1d078009d34b6229209ab6d1253aeaafcf0e8d4f
SoyuzKadel <soyuzkadel@gmail.com>
2024-10-04 17:10:17 -0400

    Menu Element added, display set none, eventlistener added(not functional)

--- aboutMe/aboutMe.js
+++ aboutMe/aboutMe.js
@@ -1,3 +1,20 @@
+
+
+//onload animation for the Project X link
 window.onload = function() {
     document.getElementById("linker").classList.add("visible");
 }
+
+
+
+
+//EventListener for the dropdownmenu "menuFirst"
+const hoverMenuFirst = document.getElementById("menufirst");
+
+hoverMenuFirst.addEventListener('mouseenter', function() {
+    document.getElementById("aboutMe2").style.display = "block";
+} );
+
+hoverMenuFirst.addEventListener('mouseleave', function() {
+    document.getElementById("aboutMe2").style.display = "none";
+} );
