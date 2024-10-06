c31bf6789343180a3f69533a65e41a4d6126328c
SoyuzKadel <soyuzkadel@gmail.com>
2024-10-06 16:47:07 -0400

    button show/off on click and hover (partial) 2nd try makes permanent inline

--- aboutMe/aboutMe.js
+++ aboutMe/aboutMe.js
@@ -9,8 +9,23 @@ window.onload = function() {
 
 
 //Menu Drop Down OnClick
+x = 0;
+function menuOnClick2() {
+    if (x%2 == 0) {
+        document.getElementById("menufirst").removeAttribute("onmouseleave");
+        document.getElementById("aboutMe2").style.display="inline";
+    }else {
+        document.getElementById("menufirst").setAttribute(onmouseleave, menuOnClick1());
+        document.getElementById("aboutMe2").style.display="none";
+    }
+    x = x + 1;
+}
+function menuOnClick1() {
+    document.getElementById("aboutMe2").style.display="none";
+    document.getElementById("aboutMe2").classList.add("menuOnClick1");
+}
+
 
 function menuOnClick() {
     document.getElementById("aboutMe2").style.display="inline";
-
 }
